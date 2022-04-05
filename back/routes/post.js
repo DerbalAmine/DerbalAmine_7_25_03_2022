ae95d697cbb54df16f0d145ef163bb526c18e6d6
Amine Derbal <muhammadderbal@outlook.com>
2022-04-06 00:52:50 +0200

    projet en cour

--- back/routes/post.js
+++ back/routes/post.js
@@ -6,15 +6,16 @@ const multer = require("../middlewares/multer-config");
 
 const postCtrl = require("../controllers/post");
 
-//MESSAGES
+//routes pour les posts 
 router.post("/", auth, multer, postCtrl.createPost);
 router.get("/", auth, multer, postCtrl.getAllPosts);
 router.get("/all/:id", auth, postCtrl.getUserPosts);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.delete("/:id", auth, postCtrl.deletePost);
 router.put("/:id", auth, multer, postCtrl.modifyPost);
+//router.delete('/admin/:id', auth, postController.deletePostByAdmin);
 
-//COMMENTAIRES
+//routes pour les commentaires 
 router.post("/:id/comment", auth, postCtrl.newComment);
 router.get("/:id/comments", auth, postCtrl.getAllComments);
 router.delete("/comment/:id", auth, postCtrl.deleteComment);
