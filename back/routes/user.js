b7b7b0900b1490e66994ad7b047083a964e648d2
Amine Derbal <muhammadderbal@outlook.com>
2022-04-14 02:23:50 +0200

    projet en cour

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
+const rateLimit = require("express-rate-limit");
 
 const userCtrl = require("../controllers/user");
 
+const passLimit = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes temps pour tester l'application
+  max: 5, //3 essais max par adresse ip
+});
+
 router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
-//router.get("/user/:id", auth, userCtrl.getOneUser);
+router.post("/login", passLimit, userCtrl.login);
 router.delete("/:id", userCtrl.deleteUser);
 
 module.exports = router;
--- front/src/main.js
+++ front/src/main.js
@@ -4,3 +4,13 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+
+
+//**Global Variables**
+//ApiUrl
+Vue.prototype.$apiUrl = 'http://localhost:3001/api';
+//User / Token
+if (localStorage.user != undefined) {
+  Vue.prototype.$token = JSON.parse(localStorage.user).token;
+  Vue.prototype.$userid = JSON.parse(localStorage.user);
+}
